183b662433afa4a280810fbcf936f1bfae6bcdfc
xyc <986327386@qq.com>
2020-03-10 19:00:21 +0800

    A

---
+++ src/views/Demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Demo
+    <br />
+    {{msg}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Demo extends Vue {
+  public msg = "";
+  public created(): void {
+    console.log(this.$route.params);
+  }
+}
+</script>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Error: NotFound.</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default class NotFound extends Vue {}
+</script>
