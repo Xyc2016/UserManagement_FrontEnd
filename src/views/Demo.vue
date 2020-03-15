b16e96fd01aad434d0b8da515f88d04a239232db
xyc <986327386@qq.com>
2020-03-15 21:21:27 +0800

    A

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -1,22 +1,41 @@
 <template>
-  <div>{{info}}</div>
+  <div>
+    {{info}}
+    <el-button type="primary" @click="getAndLogData">getAndLogData</el-button>
+  </div>
 </template>
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 @Component
 export default class Demo extends Vue {
   public info = "";
-  public async getResponseAsync(): Promise<string[]> {
-    const ar = new Array(100).fill(axios.get("/") as Promise<{ data: string }>);
-    const o = await Promise.all<string>(ar);
-    return o;
+  // public async getResponseAsync(): Promise<string[]> {
+  //   //   const ar = new Array(100).fill(axios.get("/") as Promise<{ data: string }>);
+  //   //   const o = await Promise.all<string>(ar);
+  //   //   return o;
+  // }
+  public async getData(): Promise<AxiosResponse<any>[]> {
+    const ar: Promise<AxiosResponse<any>>[] = [];
+    axios.get("http://127.0.0.1:8000/get_data");
+    for (let i = 0; i < 50; i++) {
+      ar.push(axios.get("http://127.0.0.1:8000/get_data"));
+    }
+    return await Promise.all<AxiosResponse<any>>(ar);
+  }
+  public getAndLogData(): void {
+    const begin = new Date();
+    console.log(begin, begin.getMilliseconds());
+    this.getData().then(ar => {
+      ar.map(value => {
+        console.log(value.data.dateTime);
+      });
+      const finish = new Date();
+      console.log(finish, finish.getMilliseconds());
+    });
   }
   public mounted(): void {
     axios.defaults.withCredentials = true;
-    this.getResponseAsync().then(value => {
-      console.log(value);
-    });
   }
 }
 </script>
